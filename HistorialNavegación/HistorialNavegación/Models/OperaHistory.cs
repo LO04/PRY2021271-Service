69d97428ecbf05814eb391659101bc0f7ea1dc63
Sentra <osca-ru@hotmail.com>
2022-04-24 19:21:30 -0500

    permitir hasta 30

--- HistorialNavegación/HistorialNavegación/Models/OperaHistory.cs
+++ HistorialNavegación/HistorialNavegación/Models/OperaHistory.cs
@@ -12,7 +12,7 @@ namespace HistorialNavegación.Models
         {
             path = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + @"\Opera Software\Opera Stable\History";
             query = @"Select url as URL, title as Title, Time(last_visit_time/1000000 + (strftime('%s','1601-01-01')),'unixepoch','localtime')as Time, Date(last_visit_time/1000000 + (strftime('%s','1601-01-01')),'unixepoch') as Date
-                        FROM urls ORDER BY last_visit_time DESC LIMIT 10";
+                        FROM urls ORDER BY last_visit_time DESC LIMIT 30";
             name = "Opera";
         }
     }
